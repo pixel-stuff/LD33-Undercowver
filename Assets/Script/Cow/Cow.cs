e6dcbd85e0f2623eb9efe9fa23b73e4a36276ee3
mathias <mathias@marbotic.fr>
2015-08-22 17:09:55 +0200

    Cow update

--- Assets/Script/Cow/Cow.cs
+++ Assets/Script/Cow/Cow.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum CowState{
+	Idle,
+	BeingLift,
+	Lifted,
+	Crashed,
+	Affraid
+}
+
+
 public class Cow : MonoBehaviour {
+
+	[SerializeField]
+	private bool m_isUFOCatched = false;
+
+	[SerializeField]
+	private CowState m_cowState;
 
 	// Use this for initialization
 	void Start () {
-	
+		m_cowState = CowState.Idle;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//switch(
+	}
+
+	public void setCowState(CowState newState){
+		m_cowState = newState;
+	}
+
+	public CowState getCowState(){
+		return m_cowState;
+	}
+
+	public void setIsUFOCatched(bool isUFOCatched){
+		m_isUFOCatched = isUFOCatched;
+	}
 	
+	public bool getIsUFOCatched(){
+		return m_isUFOCatched;
 	}
 }
--- Assets/Script/Cow/CowManager.cs
+++ Assets/Script/Cow/CowManager.cs
@@ -1,10 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CowManager : MonoBehaviour {
 
+	[SerializeField]
+	private GameObject m_cowPrefab;
+
+	private List<Cow> m_listCow;
+
 	// Use this for initialization
 	void Start () {
+		m_listCow = new List<Cow> ();
+
+		GameObject obj;
+		Cow cow;
+		for (int i=0; i < 4; i++) {
+			obj = (GameObject)Instantiate(m_cowPrefab,Vector3.zero,Quaternion.identity);
+			obj.transform.parent = this.transform;
+
+			cow = GetComponent<Cow>();
+			m_listCow.Add(obj.GetComponent<Cow>());
+		}
 	
 	}
 	
