a5c23c2ec9e204a90f89443f5e7d6ce2faa8b334
pplans <pierre.plans@gmail.com>
2015-08-24 17:38:32 +0200

    Flash ???

---
+++ Assets/Script/flash.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flash : MonoBehaviour {
+
+	private float m_timer = 0.0f;
+	private bool m_flash_start = false;
+
+	// Use this for initialization
+	void Start () {
+		MeshRenderer mr = GetComponent<MeshRenderer>();
+		mr.enabled = false;
+		m_flash_start = true;
+	}
+
+	public void startFlash() {
+		m_timer = 0.0f;
+		m_flash_start = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (m_flash_start) {
+			if(m_timer>1.0f) {
+				MeshRenderer mr = GetComponent<MeshRenderer>();
+				mr.enabled = true;
+			}
+			if(m_timer>2.0f) {
+				MeshRenderer mr = GetComponent<MeshRenderer>();
+				mr.enabled = false;
+			}
+			m_timer+=Time.deltaTime;
+		}
+	}
+}
