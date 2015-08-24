85f7522b51a3249d2f82db9c7dd55d4a8b60ee25
pplans <pierre.plans@gmail.com>
2015-08-25 01:36:33 +0200

    Flash sound fix

--- Assets/Script/flash.cs
+++ Assets/Script/flash.cs
@@ -8,6 +8,7 @@ public class flash : MonoBehaviour {
 	[Header("Flash Element")]
 	[SerializeField]
 	private GameObject m_flash = null;
+	private bool m_sound_played = false;
 
 	// Use this for initialization
 	void Start () {
@@ -28,7 +29,10 @@ public class flash : MonoBehaviour {
 			//Debug.Log (m_timer);
 			if(m_timer>0.9f) {
 				m_flash.SetActive (true);
-				AudioManager.Play("camera_shutter");
+			}
+			if(m_timer>1.0f && !m_sound_played) {
+				AudioManager.Play ("camera_shutter");
+				m_sound_played = true;
 			}
 			if(m_timer>1.5f) {
 				m_flash.SetActive (false);
