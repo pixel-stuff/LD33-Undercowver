53794b565959b44e402c015e0f5986e6e799332b
mathias <mathias@marbotic.fr>
2015-08-24 11:42:39 +0200

    End Scene et score fonctionnel

---
+++ Assets/Script/DisplayScoreOnEndScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayScoreOnEndScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Text> ().text =	"Level " + GameStateManager.m_instance.m_level + " completed\n" +
+											"Cow killed : " + GameStateManager.m_instance.m_CowKilled +"\n"+
+											"Temps De Jeu : " + GameStateManager.m_instance.m_playTime  +" sec\n"+
+											"Alerte started : " + GameStateManager.m_instance.m_alerteStarted;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
