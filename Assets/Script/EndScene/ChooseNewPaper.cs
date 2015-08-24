c1282e17bd15e169c2357c0975a85ca5935063b7
mathias <mathias@marbotic.fr>
2015-08-24 18:18:16 +0200

    push endScene

--- Assets/Script/EndScene/ChooseNewPaper.cs
+++ Assets/Script/EndScene/ChooseNewPaper.cs
@@ -9,8 +9,19 @@ public class ChooseNewPaper : MonoBehaviour {
 	[SerializeField]
 	private GameObject m_looseNewspaper;
 
+	public float m_alphaApplier = 0.0f;
+
+	[SerializeField]
+	private GameObject m_UIEndScene;
+
+	[SerializeField]
+	private SpriteRenderer m_cache;
+
+	private Color m_colorToAppli;
+
 	// Use this for initialization
 	void Start () {
+		m_colorToAppli = new Color (0f, 0f, 0f, 0f);
 		if(GameStateManager.getGameState() == GameState.EndSceneGameOver){
 			m_looseNewspaper.SetActive(true);
 		}else{
@@ -21,6 +32,12 @@ public class ChooseNewPaper : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		m_colorToAppli.a = m_alphaApplier;
+		m_cache.color = m_colorToAppli;
 	}
+
+	public void EndAnimation(){
+		m_UIEndScene.SetActive (true);
+	}
+
 }
