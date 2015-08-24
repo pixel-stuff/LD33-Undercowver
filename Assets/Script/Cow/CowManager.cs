d4a93ff1f5d6c1ebc5d9e4985a2ae39771db4434
mathias <mathias@marbotic.fr>
2015-08-24 15:19:06 +0200

    les cow ufo ne peuvent plus être affraid

--- Assets/Script/Cow/CowManager.cs
+++ Assets/Script/Cow/CowManager.cs
@@ -136,7 +136,7 @@ public class CowManager : MonoBehaviour {
 			//Debug.Log ("Cow Reference: " + cow.name);
 			for (int i = 0; i<m_listCow.Count; i++) {
 				//Debug.Log ("m_listCow[" + i + "]  : " + m_listCow[i]);
-				if (m_listCow [i].getId () != id && m_listCow[i].getCowState() != CowState.Dead) {
+				if (m_listCow [i].getId () != id && m_listCow[i].getCowState() != CowState.Dead && !m_listCow[i].getIsUFOCatched()) {
 					if (Vector3.Distance (m_listCow [i].transform.localPosition, cow.transform.localPosition) <= m_areaWhereCowBeAffraid) {
 						numbAffraid++;
 						m_listCow [i].setCowState (CowState.Affraid);
