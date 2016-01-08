f3bf512882ba19d9b0edbe1ab9a0d8630196f570
pplans <pierre.plans@gmail.com>
2016-01-08 19:03:50 +0100

    Light corrections
    Adding a script that updates shader parameters, updated the shader,
    which needs correct clear (as the script btw).

--- Assets/Resources/shaders/TransparentDiffuse.shader
+++ Assets/Resources/shaders/TransparentDiffuse.shader
@@ -1,26 +1,36 @@
 ﻿Shader "Custom/Transparent/Diffuse" {
 	Properties{
 		_Color("Main Color", Color) = (1,1,1,1)
+		_LightRay("Player pos", Vector) = (0,-1, 0, 0)
+		_NormalRay("Object pos", Vector) = (0, 1, 0, 0)
+		_XUVRatio("X Ratio for UV coordinates", Range(0.0, 1.0)) = 0.5
+		_YUVRatio("Y Ratio for UV coordinates", Range(0.0, 1.0)) = 0.5
 		_MainTex("Base (RGB) Trans (A)", 2D) = "white" {}
 	}
 
-	SubShader{
-	Tags{ "Queue" = "Transparent" "IgnoreProjector" = "True"/* "RenderType" = "Transparent"*/ }
-	LOD 200
-	Cull Off
-	Blend SrcAlpha OneMinusSrcAlpha
+		SubShader{
+		Tags{ "Queue" = "Transparent" "IgnoreProjector" = "True"/* "RenderType" = "Transparent"*/ }
+		LOD 200
+		Cull Off
+		Blend SrcAlpha OneMinusSrcAlpha
 
-	CGPROGRAM
-	#pragma surface surf Own alpha:fade
+		CGPROGRAM
+		#pragma surface surf Own alpha:fade
 
+	half4 _NormalRay;
+	half4 _LightRay;
+	float _XUVRatio;
+	float _YUVRatio;
 
 	half4 LightingOwn(SurfaceOutput s, half3 lightDir, half atten) {
 		//half NdotL = dot(s.Normal, lightDir);
 		half3 SRay = half3(0.0, 1.0, 0.0);
 		half3 LRay = /*_WorldSpaceLightPos0 + */lightDir;
-		half NdotL = dot(SRay, LRay);
+		half NdotL = dot(SRay, -normalize(_LightRay.xyz));
+		if (abs(NdotL) > 0.8) NdotL = 1.0;
+		else NdotL = 0.0;
 		half4 c;
-		c.rgb = s.Albedo * _LightColor0.rgb * (NdotL * atten);
+		c.rgb = s.Albedo;// *pow(NdotL, 10.9);// /* _LightColor0.rgb*/ *(NdotL * atten);
 		c.a = s.Alpha;
 		return c;
 	}
@@ -34,7 +44,19 @@
 
 	void surf(Input IN, inout SurfaceOutput o) {
 		fixed4 c = tex2D(_MainTex, IN.uv_MainTex) * _Color;
-		o.Albedo = c.rgb;
+		half3 pPos = mul(_Object2World, _LightRay).xyz;
+		half3 oPos = mul(_Object2World, _NormalRay).xyz;
+		half2 uvw = IN.uv_MainTex;// mul(_Object2World, half4(IN.uv_MainTex, 0.0f, 0.0f)).xyz;
+		half2 uv = half2(uvw.x*_XUVRatio*2.0 - 1.0, uvw.y);//half2(uvw.x*_XUVRatio*2.0-1.0, uvw.y*_YUVRatio*2.0 + 1.0);
+		half3 lDir = half3(pPos.xy - (oPos.xy + (-1.0+2.0*uv)), 0.0f);
+		half NdotL = dot(half3(0.0, 1.0, 0.0), -normalize(-lDir));
+		if (NdotL > 0.0) {
+			if (abs(NdotL) > 0.98) NdotL = 1.0;
+			else if (abs(NdotL) > 0.6) NdotL = lerp(1.0f, 0.0f, (NdotL-0.02) / 0.48);
+			else NdotL = 0.0;
+		}
+
+		o.Albedo = c.rgb * 2.0 * pow(NdotL, 10.0);
 		o.Alpha = c.a;
 	}
 	ENDCG
---
+++ Assets/Script/UpdateLightReception.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateLightReception : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+        /*GameObject player = GameObject.FindGameObjectWithTag("PlayerManager");
+        Vector4 playerPos = (new Vector4(player.transform.position.x, player.transform.position.y, player.transform.position.z, 0.0f));
+        Vector4 playerRot = (new Vector4(player.transform.rotation.x, player.transform.rotation.y, player.transform.rotation.z, 0.0f));
+        Vector4 playerLRay = (player.transform.rotation*(new Vector4(0.0f, -1.0f, 0.0f, 0.0f)));
+        Vector4 oNRay = (new Vector4(transform.position.x, transform.position.y, transform.position.z, 0.0f))+(new Vector4(0.0f, 1.0f, 0.0f, 0.0f));
+        Vector4 LRay = new Vector4(transform.position.x - player.transform.position.x,
+            transform.position.y - player.transform.position.y,
+            0.0f,
+            0.0f);
+        // (new Vector4(transform.position.x, transform.position.y + 1.0f, transform.position.z, 0.0f));
+        this.gameObject.GetComponent<SpriteRenderer>().material.SetVector("_NormalRay", playerPos+playerLRay);
+        this.gameObject.GetComponent<SpriteRenderer>().material.SetVector("_LightRay", LRay);*/
+        GameObject player = GameObject.FindGameObjectWithTag("PlayerManager");
+        Vector4 pPos = new Vector4(player.transform.position.x,
+             player.transform.position.y,
+             0.0f,
+             0.0f);
+        Vector4 oPos = new Vector4(transform.position.x,
+             transform.position.y,
+             0.0f,
+             0.0f);
+        Texture tex2D = this.gameObject.GetComponent<SpriteRenderer>().material.GetTexture("_MainTex");
+        if (tex2D != null) {
+            float ratio = tex2D.width / tex2D.height;
+            this.gameObject.GetComponent<SpriteRenderer>().material.SetFloat("_XUVRatio", ratio);
+            ratio = tex2D.height / tex2D.width;
+            this.gameObject.GetComponent<SpriteRenderer>().material.SetFloat("_YUVRatio", ratio);
+        }
+        this.gameObject.GetComponent<SpriteRenderer>().material.SetVector("_NormalRay", oPos);
+        this.gameObject.GetComponent<SpriteRenderer>().material.SetVector("_LightRay", pPos);
+    }
+}
